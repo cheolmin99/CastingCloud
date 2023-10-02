470171133a2692de17cbe4027453ffb394519f63
Cheolmin99 <cjfals8570@naver.com>
2023-10-02 23:30:20 +0900

    websecurity 설정 + 추후 보완해야 함.

---
+++ CastingCloud-Back/src/main/java/com/example/castingCloud/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.castingCloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.castingCloud.filter.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors().and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/*"/* 인증이 된 url */).authenticated()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/**" /* get 메서드 허용 url */).permitAll()
+                .anyRequest().authenticated().and()
+                .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+    
+
+}
