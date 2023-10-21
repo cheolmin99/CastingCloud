ae90778dc7afdff5b81573782c88c19f79a8216f
Cheolmin99 <cjfals8570@naver.com>
2023-10-21 13:24:02 +0900

    로그인 페이지 구성중?

--- CastingCloud-Front/front/src/views/LandingPageView/index.tsx
+++ CastingCloud-Front/front/src/views/LandingPageView/index.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react"
+import './index.css';
+
 export default function LandingPageView() {
+
+    const [ loginView, setLoginView ] = useState<boolean>(true);
 
     return (
         <>
+            <div className="landingPageContainor">
+                <div className="left-box">
+                    여긴 랜딩페이지에 관한 뭐가 들어가겠지
+                </div>
+                <div className="right-box">
+                    <div className="login-box">
+                        <div className="login">
+
+                        </div>
+                        <input className="email" type="email" />
+                        <div className="option" typeof="option">
+                            @
+                                <select className="option-box" name="order">
+                                    <option value="naver">naver.com</option>
+                                    <option value="google">google.com</option>
+                                    <option value="kakao">kakao.com</option>
+                                    <option value="daum">daum.net</option>
+                                </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
